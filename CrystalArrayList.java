1e8b3c275b0e667df17019aea98837dfc54b2261
CaptainRock <57752876+CaptainRock@users.noreply.github.com>
2023-01-23 11:33:02 +0530

    Add files via upload

---
+++ CrystalArrayList.java
@@ -0,0 +1,14 @@
+package com.bj.enterprise.simple.simple.model;
+
+import java.util.ArrayList;
+
+public  class CrystalArrayList {
+
+    public static ArrayList<String> crystals = new ArrayList<>();
+
+    public CrystalArrayList(){
+
+    }
+
+
+}
